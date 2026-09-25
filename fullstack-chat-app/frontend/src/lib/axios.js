2582d1ef2af4ea005b174dca9fd493feb426f007
Mahin Ahmad <35218856+9mAhmad@users.noreply.github.com>
2026-09-25 15:48:12 +0600

    Fix baseURL formatting in axios instance

--- fullstack-chat-app/frontend/src/lib/axios.js
+++ fullstack-chat-app/frontend/src/lib/axios.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8088" + "/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8088/api" : "/api",
   withCredentials: true,
 });
 
