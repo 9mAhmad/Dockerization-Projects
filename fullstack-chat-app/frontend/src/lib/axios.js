e14e6c3901c17e5bf420c1c051cd1d0cc7aec726
Mahin Ahmad <35218856+9mAhmad@users.noreply.github.com>
2026-09-25 15:51:12 +0600

    removed dotenv

--- fullstack-chat-app/frontend/src/lib/axios.js
+++ fullstack-chat-app/frontend/src/lib/axios.js
@@ -1,10 +1,5 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-
-
+   
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:8088/api" : "/api",
   withCredentials: true,
