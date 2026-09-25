79fc379689b0dbf963e19bc53c515dd2a46e7738
Mahin Ahmad <35218856+9mAhmad@users.noreply.github.com>
2026-09-25 15:07:45 +0600

    fix: frontend api fetch code now uses VITE_API_URL

--- fullstack-chat-app/frontend/src/lib/axios.js
+++ fullstack-chat-app/frontend/src/lib/axios.js
@@ -1,6 +1,13 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? process.env.VITE_API_URL + "/api" : "/api",
   withCredentials: true,
 });
+
+
